a050ea3916ede4e64c1cf305d46fbceb7b4e4295
Kishore1896 <114785064+Kishore1896@users.noreply.github.com>
2024-09-09 14:01:27 -0500

    Delete src directory

--- src/components/Skills/SkillCard.jsx
+++
@@ -1,9 +0,0 @@
-import "./Skills.css";
-export default function SkillCard({skillName, skillUrl}) {
-  return (
-    <div className="skill">
-    <img src={skillUrl} alt={skillName} loading="lazy"/>
-    <p>{skillName}</p>
-    </div>
-  )
-}
